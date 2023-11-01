b930b226080661a061749da3c7d3e43055f2b41f
Shivam Shrivastava <shivam.shrivastava1006@gmail.com>
2023-11-01 12:41:32 +0530

    Update one.js

--- openai_testing/one.js
+++ openai_testing/one.js
@@ -10,7 +10,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
    organization: "org-T8Su9wEl3AA1mLKGvOjGDwAc",
-   apiKey: "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",
+   apiKey: "data.openaiApiKey",
 });
 
 const openai = new OpenAIApi(configuration);
